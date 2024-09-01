4988409447a4500540118a4aa8c93c725b433b86
root <gabgebgib@gmail.com>
2024-09-01 13:58:10 +0800

    Remove celery experiments, rename main script for adding tasks to Celery worker

--- celery/run_task.py
+++
@@ -1,8 +0,0 @@
-# run_task.py
-from celery_app import hello
-
-# Call the task asynchronously
-result = hello.delay()
-
-# Get the result (this will block until the task is completed)
-print("Task result:", result.get())
